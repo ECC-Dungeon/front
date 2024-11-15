3e30a6c7df067786e55c7fbdc0aeb5a24b9ebcc9
shimizu <noon3m.op.sh@gmail.com>
2024-11-15 19:15:42 +0900

    [add]Axiosを使用した仮、のAPIクライアントを実装し、リクエストとレスポンスのインターセプターを追加

---
+++ src/lib/api-client.ts
@@ -0,0 +1,36 @@
+import Axios, { InternalAxiosRequestConfig } from 'axios';
+
+import { paths } from '@/config/paths';
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+  if (config.headers) {
+    config.headers.Accept = 'application/json';
+  }
+
+  config.withCredentials = true;
+  return config;
+}
+
+export const api = Axios.create({
+  baseURL: 'http://localhost:8080',
+});
+
+api.interceptors.request.use(authRequestInterceptor);
+api.interceptors.response.use(
+  (response) => {
+    return response.data;
+  },
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    alert(message);
+
+    if (error.response?.status === 401) {
+      const searchParams = new URLSearchParams();
+      const redirectTo =
+        searchParams.get('redirectTo') || window.location.pathname;
+      window.location.href = paths.auth.login.getHref(redirectTo);
+    }
+
+    return Promise.reject(error);
+  },
+);
