8f633c2c455ca7e8676ede1ace3a080b96613ec2
shimizu <107845401+noonyuu@users.noreply.github.com>
2024-11-18 12:45:42 +0900

    Feature/enviroment preparation (#3)
    * [change]パスエイリアスを設定
    
    * [add]storybookを追加したり必要なパッケージをインストールした
    
    * [add]仮のエンティティを作成
    
    * [add]クエリ設定と型定義を追加
    
    * [add]各ルートを管理するオブジェクトを作成
    
    * [add]ルーティングとエラーハンドリングの仕組みを構築するためのコンポーネント
    
    * [remove]いらないから削除
    
    * [add]仮置きの各画面のファイル
    
    * [add]Axiosを使用した仮、のAPIクライアントを実装し、リクエストとレスポンスのインターセプターを追加
    
    * [add]認証ロジックを統一し、ユーザー情報取得、ログイン、ログアウト機能を実装するコンポーネントを追加
    
    * [add]React Queryとエラーバウンダリを使用したプロバイダーコンポーネントを追加
    
    * [add]プロバイダーコンポーネントを追加し、React Routerの設定を更新
    
    * [add]環境変数検証用
    
    * [add]Storybookの設定を更新し、追加のアドオンを導入、preview.tsを削除し、preview.tsxを新規作成
    
    * [add]READMEにフロントエンドの構成を追加し、package.jsonにモックサーバー起動スクリプトと依存関係を追加、tailwind.config.jsから不要な行を削除
    
    * Delete package-lock.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,14 +3,23 @@ import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/node-logger',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
   },
 };
 export default config;
