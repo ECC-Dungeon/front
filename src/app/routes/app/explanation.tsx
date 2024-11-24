8a97415fa69353ce2b63f396c2a37d7af3030668
shimizu <noon3m.op.sh@gmail.com>
2024-11-24 22:56:24 +0900

    [add]説明画面補足

--- src/app/routes/app/explanation.tsx
+++ src/app/routes/app/explanation.tsx
@@ -1,8 +1,22 @@
+import { InputTeam } from '@/feature/components/explanation/input-team';
 import { useUser } from '@/lib/auth';
+import { getTeamName } from '@/lib/team-name';
 
 export const ExplanationRoute = () => {
-   const user = useUser();
+  const user = useUser();
+  if (!user.data) return null;
 
-   if (!user.data) return null;
-  return <div>Explanation</div>;
+  const teamName = getTeamName();
+
+  return (
+    <section>{teamName ? <p>チーム名: {teamName}</p> : <InputTeam />}</section>
+  );
 };
+
+/**
+ * 後で消す
+ * 説明用のファイル
+ * ・初期状態はチーム名入力フォーム
+ * ・チーム名が登録されている場合は説明を表示
+ * ・サーバーから開始状況を取得
+ */
