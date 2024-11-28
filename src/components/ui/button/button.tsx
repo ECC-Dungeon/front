26c0e8ffd863dc51d720476c5e8963fef26925ca
yuki <2240022@ecc.ac.jp>
2024-11-28 11:22:16 +0900

    add ボタンとグラデーションのコンポーネント

---
+++ src/components/ui/button/button.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
+
+export const buttonVariants = cva(
+  `bg-gradient-to-r from-[#543F2C] to-[#2A2416] h-20`,
+  {
+    variants: {
+      size: {
+        small: `w-52`,
+        medium: `w-[304px]`,
+      },
+    },
+    defaultVariants: {
+      size: 'small',
+    },
+  },
+);
+
+export const innerVariants = cva(
+  `bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-[#63832D] to-[#485922] text-white text-2xl font-semibold border-solid border-4 border-[#526428] mx-auto h-16 flex items-center justify-center`,
+  {
+    variants: {
+      size: {
+        small: `w-48`,
+        medium: `w-72`,
+      },
+    },
+    defaultVariants: {
+      size: 'small',
+    },
+  },
+);
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  onClick: () => void; // ボタンがクリックされたときの処理
+  label: string; // ボタンに表示するテキスト
+}
+
+// ボタンコンポーネント
+const Button: React.FC<ButtonProps> = ({ onClick, label, size = 'small' }) => {
+  return (
+    <button className={`${buttonVariants({ size })}`} onClick={onClick}>
+      <div className={`${innerVariants({ size })}`}>{label}</div>
+    </button>
+  );
+};
+
+export default Button;
