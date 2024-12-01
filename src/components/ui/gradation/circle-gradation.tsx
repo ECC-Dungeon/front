864f0b67f26f22bcee43eef94c6a23c2891fc304
木下 <171928933+yukikinoshita09@users.noreply.github.com>
2024-12-02 03:13:23 +0900

    add QR獲得画面作成 (#17)
    * add QR獲得画面作成
    
    * refactor export方法変更
    
    ---------
    
    Co-authored-by: yuki <2240022@ecc.ac.jp>

--- src/components/ui/gradation/circle-gradation.tsx
+++ src/components/ui/gradation/circle-gradation.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
-// 円形グラデーションコンポーネント
-const CircleGradation: React.FC = () => {
-  return (
-    <div className="absolute h-96 w-96 rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#fdd15926] via-[#fdd1590f] to-[#fdd15906] blur-md filter"></div>
-  );
+type CircleGradationProps = {
+  children?: React.ReactNode;
 };
 
-export default CircleGradation;
+// 円形グラデーションコンポーネント
+export const CircleGradation: React.FC<CircleGradationProps> = ({ children }) => {
+  return (
+    <div className="absolute h-[520px] w-[520px] flex-col items-center justify-center rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#fdd15926] via-[#fdd1590f] to-[#fdd15906] filter backdrop-blur-lg">
+      {children}
+    </div>
+  );
+};
