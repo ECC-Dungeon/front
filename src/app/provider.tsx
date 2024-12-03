bb16077d27b775f8531c0cf6e79fd5c59a45b5a2
shimizu <noon3m.op.sh@gmail.com>
2024-12-03 16:29:49 +0900

    [change]AuthLoaderを削除し、Suspenseのフォールバックを更新

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,10 +1,9 @@
-import React, { Suspense, useState } from 'react';
+import { Suspense, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 
 import { MainError } from '@/components/errors/main';
 import { queryConfig } from '@/lib/query';
-import { AuthLoader } from '@/lib/auth';
 
 type ProviderProps = {
   children: React.ReactNode;
@@ -19,20 +18,12 @@ export const Provider = ({ children }: ProviderProps) => {
   );
 
   return (
-    <Suspense fallback={<div></div>}>
+    <Suspense fallback={<div>...</div>}>
       {/* sスピナーを表示する */}
       {/* エラー画面を作成する */}
       <ErrorBoundary FallbackComponent={MainError}>
         <QueryClientProvider client={queryClient}>
-          <AuthLoader
-            renderLoading={() => (
-              <div className="flex h-screen w-screen items-center justify-center">
-                {/* スピナーを表示する */}
-              </div>
-            )}
-          >
-            {children}
-          </AuthLoader>
+          {children}
         </QueryClientProvider>
       </ErrorBoundary>
     </Suspense>
