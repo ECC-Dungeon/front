b9e64b37c701a0a035ef21b14488aaacaeafc62e
shimizu <noon3m.op.sh@gmail.com>
2024-12-04 11:37:08 +0900

    [change]QRスキャンコンポーネントを更新し、QRコードの結果をローカルストレージに保存して成功時にコールバックを呼び出すロジックを追加。不要なフォーム部分を削除。

--- src/feature/login/components/qr-scan.tsx
+++ src/feature/login/components/qr-scan.tsx
@@ -1,6 +1,5 @@
 import { useQrScanner } from '@/hooks/use-qr-scanner';
-import { Form, Input } from '@/components/ui/form';
-import { useLogin, loginInputSchema } from '@/lib/auth';
+import { useEffect } from 'react';
 
 type LoginFormProps = {
   onSuccess: () => void;
@@ -9,9 +8,12 @@ type LoginFormProps = {
 export const QrScan = ({ onSuccess }: LoginFormProps) => {
   const { videoRef, canvasRef, result, error } = useQrScanner();
 
-  const login = useLogin({
-    onSuccess,
-  });
+  useEffect(() => {
+    if (result) {
+      localStorage.setItem('gameToken', result);
+      onSuccess();
+    }
+  }, [result]);
 
   return (
     <section>
@@ -37,45 +39,6 @@ export const QrScan = ({ onSuccess }: LoginFormProps) => {
       {!result && !error && (
         // TODO: ローディングコンポーネントに切り替える
         <p className="text-center text-gray-500">読み取り中...</p>
-      )}
-      {result && (
-        <div>
-          <p className="text-center text-gray-500">{result}</p>
-          {/* TODO: 仮のフォーム */}
-          <Form
-            onSubmit={(values) => {
-              login.mutate(values);
-            }}
-            schema={loginInputSchema}
-          >
-            {({ register, formState }) => (
-              <>
-                <Input
-                  type="email"
-                  label="Email Address"
-                  error={formState.errors['email']}
-                  registration={register('email')}
-                  // value={"222@gmail.com"}
-                />
-                <Input
-                  type="password"
-                  label="Password"
-                  error={formState.errors['password']}
-                  registration={register('password')}
-                  value={"testtest"}
-                />
-                <div>
-                  <button
-                    type="submit"
-                    className="w-full"
-                  >
-                    Log in
-                  </button>
-                </div>
-              </>
-            )}
-          </Form>
-        </div>
       )}
     </section>
   );
