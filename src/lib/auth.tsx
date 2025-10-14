ccbaec7151e6045588255f7a55bb1eae9b392532
noonyuu <noon3m.op.sh@gmail.com>
2025-10-14 14:03:06 +0900

    feat:開発環境を調整

--- src/lib/auth.tsx
+++ src/lib/auth.tsx
@@ -2,9 +2,15 @@ import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { paths } from '@/config/paths';
+import { env } from '@/config/env';
 
 // 認証が必要なページを保護するコンポーネント
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  // 開発環境で認証をスキップする
+  if (env.SKIP_AUTH) {
+    return children;
+  }
+
   try {
     const gameToken = localStorage.getItem('gameToken');
     if (!gameToken) {
