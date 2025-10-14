fe6a6b3751ed336fbbb02fac6f6cb575e6e4ef6d
noonyuu <noon3m.op.sh@gmail.com>
2025-10-14 14:24:23 +0900

    feat: ログインフォームにコンテンツレイアウトとモーダルのスタイルを追加

--- src/feature/login/components/login-form.tsx
+++ src/feature/login/components/login-form.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { QrScan } from './qr-scan';
 import Button from '@/components/ui/button/button';
+import { ContentLayout } from '@/components/layouts/content-layout';
+import { MdCancel } from 'react-icons/md';
 
 type LoginFormProps = {
   onSuccess: () => void;
@@ -18,31 +20,36 @@ export const LoginForm = ({ onSuccess }: LoginFormProps) => {
   };
 
   return (
-    <section className="flex h-screen flex-col items-center justify-center gap-4">
-      <div>
-        {/* TODO: ロゴに差し替える */}
-        <img src="/ecc-dungeon-logo.webp" alt="ECCダンジョンメインロゴ" />{' '}
-      </div>
-      <Button onClick={openModal} children="認証QRコードを読み取る" />
-      <Modal
-        isOpen={modal}
-        style={{
-          content: {
-            height: '360px',
-            padding: '0',
-            margin: 'auto',
-            display: 'flex',
-            justifyContent: 'center',
-            border: 'none',
-          },
-        }}
-      >
-        <div className="">
-          {/* closeを差し替える */}
-          <button onClick={closeModal}>close</button>
-          <QrScan onSuccess={onSuccess} />
+    <ContentLayout>
+      <div className="h-screen bg-[radial-gradient(circle_at_center,_#323232cc_0%,_#323232ff_100%)]">
+        <div className="flex h-screen flex-col items-center justify-center gap-4">
+          <div>
+            <img src="/ecc-dungeon-logo.webp" alt="ECCダンジョンメインロゴ" />
+          </div>
+          <Button onClick={openModal} children="認証QRコードを読み取る" />
+          <Modal
+            isOpen={modal}
+            style={{
+              content: {
+                width: '360px',
+                height: '360px',
+                padding: '0',
+                margin: 'auto',
+                display: 'flex',
+                justifyContent: 'center',
+                border: 'none',
+              },
+            }}
+          >
+            <div>
+              <button onClick={closeModal}>
+                <MdCancel />
+              </button>
+              <QrScan onSuccess={onSuccess} />
+            </div>
+          </Modal>
         </div>
-      </Modal>
-    </section>
+      </div>
+    </ContentLayout>
   );
 };
