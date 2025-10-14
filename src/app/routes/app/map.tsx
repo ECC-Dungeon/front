89edafdd4c19bca6f0cac1ba9596dc30afb659e5
noonyuu <noon3m.op.sh@gmail.com>
2025-10-14 15:40:01 +0900

    feat: 不要なdivを削除

--- src/app/routes/app/map.tsx
+++ src/app/routes/app/map.tsx
@@ -7,20 +7,18 @@ import ReaderButton from '@/feature/map/components/reader-button.tsx';
 export const MapRoute = () => {
   return (
     <ContentLayout>
-      <div>
-        <div className="flex items-center justify-center space-x-12 pb-7 pt-20">
-          <div className="text-center text-white">
-            <p className="">経過時間</p>
-            <div className="text-5xl">
-              <Stopwatch />
-            </div>
+      <div className="flex items-center justify-center space-x-12 pt-20 pb-7">
+        <div className="text-center text-white">
+          <p className="">経過時間</p>
+          <div className="text-5xl">
+            <Stopwatch />
           </div>
-          <Progress progress={0} /> {/*進捗が0の場合*/}
         </div>
-        <MapTable />
-        <div className="mt-10 flex items-center justify-center">
-          <ReaderButton />
-        </div>
+        <Progress progress={0} /> {/*進捗が0の場合*/}
+      </div>
+      <MapTable />
+      <div className="mt-10 flex items-center justify-center">
+        <ReaderButton />
       </div>
     </ContentLayout>
   );
