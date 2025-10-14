4a915a61b15b7baa361e48c00e5b6e3811be542d
noonyuu <noon3m.op.sh@gmail.com>
2025-10-14 15:01:51 +0900

    chore: tailwind ^3.4.14 → ^4.1.14

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [react(), viteTsconfigPaths(), tailwindcss()],
 });
