1f583fb5334f9f77b93406c8d101dbe26d896c5f
noonyuu <noon3m.op.sh@gmail.com>
2025-11-06 01:13:39 +0900

    feat: はみ出る不具合を直した

--- src/components/ui/gradation/circle-gradation.tsx
+++ src/components/ui/gradation/circle-gradation.tsx
@@ -5,10 +5,14 @@ type CircleGradationProps = {
 };
 
 // 円形グラデーションコンポーネント
-export const CircleGradation: React.FC<CircleGradationProps> = ({ children }) => {
+export const CircleGradation: React.FC<CircleGradationProps> = ({
+  children,
+}) => {
   return (
-    <div className="absolute h-[520px] w-[520px] flex-col items-center justify-center rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#fdd15926] via-[#fdd1590f] to-[#fdd15906] filter backdrop-blur-lg">
-      {children}
+    <div className="relative size-full overflow-hidden">
+      <div className="absolute top-1/2 left-1/2 h-[520px] w-[520px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-full bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#fdd15926] via-[#fdd1590f] to-[#fdd15906] filter backdrop-blur-lg">
+        {children}
+      </div>
     </div>
   );
 };
