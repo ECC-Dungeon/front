cb5b66eab5a8051bb454945f1521735742a7fa49
noonyuu <noon3m.op.sh@gmail.com>
2026-02-08 02:05:04 +0900

    fix: mock修正

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -5,6 +5,12 @@ export const enableMocking = async () => {
     const { worker } = await import('./browser');
     const { initializeDb } = await import('./db');
     await initializeDb();
+
+    // mock用のデフォルトgameIdをlocalStorageにセット
+    if (!localStorage.getItem('gameId')) {
+      localStorage.setItem('gameId', 'f7f3ea7-562c-438c-837a-771688919149');
+    }
+
     return worker.start({
       serviceWorker: {
         url: '/user/mockServiceWorker.js',
